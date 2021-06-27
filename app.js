cc7e1f1f59057cfbadc73c5d9f21532673bf19a2
Atifba86 <70482315+Atifba86@users.noreply.github.com>
2021-06-27 16:07:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// var a,b,c;
+
+// legal variables
+// var abc;
+// var Ab;
+// var bCa;
+// var a_b;
+// var a$a;
+// var _ab;
+
+// illegal variables
+// var &ab;
+// var -ab;
+// var 'ab';
+// var 123;
+
+// document.write("<h1>Rules for naming JS Varaiable</h1><br><br>")
+// document.write("Variable Name Contain $ , _ , numbers<br>")
+// document.write("Variable Name starts with a $ , _ , letter<br>")
+// document.write("Variable Name are case Sensetive<br>")
+// document.write("Variable Name should not be a JS Keywords<br>")
+
